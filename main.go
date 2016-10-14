5b23367f0958916a949e2855d49f5e2eefdb0848
osxfcn <skrtroot@protonmail.com>
2016-10-14 20:55:57 +0300

    variadic filter

--- main.go
+++ main.go
@@ -26,23 +26,18 @@ func main() {
 	xd := time.Now()
 	fmt.Println("BEFORE X", time.Since(start))
 	//X(hh, "Sun, 1 Oct 2016 18:08:09 +0300")
-	rex := hh.DateRangeFilter("", "Sun, 2 Oct 2016 18:08:09 +0300", []server.Predicate{func(commit server.GitCommitResponse) bool {
+	rex := hh.DateRangeFilter("", "", []server.Predicate{
+		func(commit server.GitCommitResponse) bool {
 			return commit.Author.Name == "AveVlad"
-	},})
+		},
+		func(commit server.GitCommitResponse) bool {
+			return commit.CommitID == "24d4f30ce4001a2ab9a37005a1d2c6537ecd3ac0"
+		}}...)
 	//rexx := hh.DateRangeFilter("Sun, 2 Oct 2016 18:08:09 +0300", "")
 	fmt.Println(len(rex))
 	//fmt.Println(len(rexx))
 	fmt.Println(len(hh))
 	fmt.Println("AFTER X", time.Since(xd))
-
-	//
-	//xd2 := time.Now()
-	//fmt.Println("BEFORE X2", time.Since(start))
-	//X(hh, "Sun, 2 Oct 2016 18:08:09 +0300")
-	//fmt.Println("AFTER X2", time.Since(xd2))
-	//fmt.Print(hh[0].VerificationFlag)
-
-	fmt.Println("TSSS")
 	fmt.Println("//////////\\///313/23////d/f/vc/c/vc/xv/cx/vd/fas/df/ds/fa")
 	fmt.Println("//////////\\///313/23////d/f/vc/c/vc/xv/cx/vd/fas/df/ds/fa")
 	fmt.Println("//////////\\///313/23////d/f/vc/c/vc/xv/cx/vd/fas/df/ds/fa")
@@ -50,47 +45,4 @@ func main() {
 	//fmt.Println(len(XX(hh, "Sun, 2 Oct 2016 18:08:09 +0300", "")))
 
 	//fmt.Print(len(XX(hh, "", "")))
-}
-
-func DateFilter(commitArray []server.GitCommitResponse, dateStart string, dateEnd string) []server.GitCommitResponse {
-	res := []server.GitCommitResponse{}
-
-	timeStart, errNoStart := server.GitDateTime(dateStart)
-	timeEnd, errNoEnd := server.GitDateTime(dateEnd)
-
-	switch true {
-	case errNoStart != nil && errNoEnd != nil:
-		fmt.Println("CASE NO INTERVAL")
-		return commitArray
-	case errNoStart != nil && errNoEnd == nil:
-		fmt.Println("CASE NO START")
-		for _, v := range commitArray {
-			commitDate, err := server.GitDateTime(v.Author.Date)
-			if commitDate.Unix() <= timeEnd.Unix() && err == nil {
-				//fmt.Println(k, v.Author.Date)
-				res = append(res, v)
-			}
-		}
-	case errNoStart == nil && errNoEnd != nil:
-		fmt.Println("CASE NO END")
-		for _, v := range commitArray {
-			commitDate, err := server.GitDateTime(v.Author.Date)
-			if commitDate.Unix() >= timeStart.Unix() && err == nil {
-				//fmt.Println(k, v.Author.Date)
-				res = append(res, v)
-			}
-		}
-	case errNoStart == nil && errNoEnd == nil:
-		fmt.Println("CASE EXACT")
-		for _, v := range commitArray {
-			commitDate, err := server.GitDateTime(v.Author.Date)
-			if commitDate.Unix() >= timeStart.Unix() && commitDate.Unix() <= timeEnd.Unix() && err == nil {
-				//fmt.Println(k, v.Author.Date)
-				res = append(res, v)
-			}
-		}
-	}
-
-	fmt.Println("LENENELELENELN", len(res))
-	return res
 }
--- server/filters.go
+++ server/filters.go
@@ -4,12 +4,12 @@ import "fmt"
 
 type Predicate func(a GitCommitResponse) bool
 
-func (commitsSlice GitCommitResponseSlice) Filter(f []Predicate) GitCommitResponseSlice {
+func (commitsSlice GitCommitResponseSlice) Filter(filters ...Predicate) GitCommitResponseSlice {
 	res := GitCommitResponseSlice{}
 	for _, v := range commitsSlice {
 		isSatisfied := true
-		for _, ff := range f {
-			if !ff(v) {
+		for _, filter := range filters {
+			if !filter(v) {
 				isSatisfied = false
 			}
 		}
@@ -21,14 +21,18 @@ func (commitsSlice GitCommitResponseSlice) Filter(f []Predicate) GitCommitRespon
 	return res
 }
 
-func (commitArray GitCommitResponseSlice) DateRangeFilter(dateStart string, dateEnd string, f []Predicate) GitCommitResponseSlice {
+func (commitArray GitCommitResponseSlice) DateRangeFilter(dateStart string, dateEnd string, f ...Predicate) GitCommitResponseSlice {
 	res := GitCommitResponseSlice{}
 	timeStart, errNoStart := GitDateTime(dateStart)
 	timeEnd, errNoEnd := GitDateTime(dateEnd)
 	switch true {
 	case errNoStart != nil && errNoEnd != nil:
 		fmt.Println("CASE NO INTERVAL")
-		return commitArray
+		if f != nil {
+			res = commitArray.Filter(f...)
+		} else {
+			return commitArray
+		}
 	case errNoStart != nil && errNoEnd == nil:
 		fmt.Println("CASE NO START")
 		res = commitArray.Filter(append(f, func(commit GitCommitResponse) bool {
@@ -37,25 +41,25 @@ func (commitArray GitCommitResponseSlice) DateRangeFilter(dateStart string, date
 				return commitTime.Unix() <= timeEnd.Unix()
 			}
 			return false
-		},))
+		})...)
 	case errNoStart == nil && errNoEnd != nil:
 		fmt.Println("CASE NO END")
-		res = commitArray.Filter([]Predicate{func(commit GitCommitResponse) bool {
+		res = commitArray.Filter(append(f, func(commit GitCommitResponse) bool {
 			commitTime, err := GitDateTime(commit.Author.Date)
 			if err == nil {
 				return commitTime.Unix() >= timeStart.Unix()
 			}
 			return false
-		},})
+		})...)
 	case errNoStart == nil && errNoEnd == nil:
 		fmt.Println("CASE EXACT")
-		res = commitArray.Filter([]Predicate{func(commit GitCommitResponse) bool {
+		res = commitArray.Filter(append(f, func(commit GitCommitResponse) bool {
 			commitTime, err := GitDateTime(commit.Author.Date)
 			if err == nil {
 				return commitTime.Unix() >= timeStart.Unix() && commitTime.Unix() <= timeEnd.Unix()
 			}
 			return false
-		},})
+		})...)
 	}
 	fmt.Println("LENENELELENELN", len(res))
 	return res
